be02286a0eb66c9ab449d2ecd065182eb80deeb6
Kevin Wellmann <kevin@wellmann.io>
2023-09-29 22:56:02 +0200

    add confirmation prompt

--- cli.js
+++ cli.js
@@ -3,6 +3,9 @@
 const { program } = require('commander');
 const extractDeclarations = require('./src/extractDeclarations');
 const packageJson = require('./package.json');
+const path = require('path');
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
 const transformDeclarations = require('./src/transformDeclarations');
 const transformDesignTokens=require('./src/transformDesignTokens');
 const writeThemeJson = require('./src/writeThemeJson');
@@ -15,10 +18,22 @@ program
     .option('--fontSizeDivisor <value>', 'calculate mobile font size based on provided deskto value', '1.2')
 	  .parse();
 
-(async function () {
+const themeJsonPath = path.join(process.cwd(), 'theme.json');
+const rl = readline.createInterface({ input, output });
+rl.question(`\x1b[2m This script will manipulate \x1b[0m${themeJsonPath}\x1b[2m. \x1b[1mProceed? \x1b[0m`, (answer) => {
+  if (answer !='y' && answer !='yes') {
+    process.exit();
+  }
+
+  run();
+
+  rl.close();
+});
+
+async function run() {
     const declarations = await extractDeclarations(program.opts());
     const designTokens = transformDeclarations(program.opts(), declarations);
     const transformedDesignTokens = transformDesignTokens(program.opts(), designTokens);
 
     writeThemeJson(program.opts(), transformedDesignTokens);
-})();
+};
